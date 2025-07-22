24f064f7db82f4e63e7cbee8f08a183110baa6a5
Rua MELHEM <153845904+RUAMELHEM@users.noreply.github.com>
2025-07-22 13:45:48 +0300

    admin.cs

---
+++ admin.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nttttttpOdevi
+{
+    class admin
+    {
+        public static List<Musteri> Musteriler = new List<Musteri>();
+        public static List<Musteri> SecilenMusteriler = new List<Musteri>();
+
+    }
+}
